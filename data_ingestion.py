b31015599cb3434ece02d0eb7d32a5f09ecbb96b
DESKTOP-B2QPHF7\lucia <luciahrovatin@gmail.com>
2021-08-19 18:31:26 +0200

    Updating functions

--- data_ingestion.py
+++ data_ingestion.py
@@ -14,7 +14,7 @@ from shapely.geometry import Polygon
 from shapely.geometry import LineString
 from shapely.ops import split
 from tqdm import tqdm
-import open3d as o3d
+#import open3d as o3d
 
 def unzip_file(zipfolder: str):
     """
@@ -92,36 +92,40 @@ def extract_objects(my_path: str):
     if element.endswith(".jpeg") and element[-6].isnumeric():
         name_image = element.split("/")[-1]
         boxes = bbox_image(name_image)
-        polylist = []
         for box in boxes:
-            if boxes[box][0][0] < 0 or boxes[box][0][0] > 1023:
-                if boxes[box][0][0] < 0:
-                    boxes[box][0][0] = 0
-                else:
-                    boxes[box][0][0] = 1022
-            if boxes[box][0][1] < 0 or boxes[box][0][1] > 1023:
-                if boxes[box][0][1] < 0:
-                    boxes[box][0][1] = 0
-                else:
-                    boxes[box][0][1] = 1022
-            min_x, max_x = boxes[box][0][0], boxes[box][0][0]
-            min_y, max_y = boxes[box][0][1], boxes[box][0][1]
+            if len(set(tuple(x) for x in boxes[box])) == len(boxes[box]):
+                if boxes[box][0][0] < 0 or boxes[box][0][0] > 1023:
+                    if boxes[box][0][0] < 0:
+                        boxes[box][0][0] = 0
+                    else:
+                        boxes[box][0][0] = 1023
+                if boxes[box][0][1] < 0 or boxes[box][0][1] > 1023:
+                    if boxes[box][0][1] < 0:
+                        boxes[box][0][1] = 0
+                    else:
+                        boxes[box][0][1] = 1023
+                min_x, max_x = boxes[box][0][0], boxes[box][0][0]
+                min_y, max_y = boxes[box][0][1], boxes[box][0][1]
 
-            print("these:", max_y, min_y, min_x, max_x)
-            for vertices in boxes[box]:
-                if vertices[0] >= max_x and vertices[0] > 0 and vertices[0] < 1023:
-                    max_x = vertices[0]
-                elif vertices[0] < min_x and vertices[0] > 0 and vertices[0] < 1023:
-                    min_x = vertices[0]
-                if vertices[1] >= max_y and vertices[1] > 0 and vertices[1] < 1023:
-                    max_y = vertices[1]
-                elif vertices[1] < min_y and vertices[1] > 0 and vertices[0] < 1023:
-                    min_y = vertices[1]
-            print("these:", min_y, min_y + (max_y - min_y), "and", min_x, min_x + (max_x - min_x))
-            im = cv2.imread(my_path , cv2.IMREAD_COLOR)
-            rectangle = im[min_y: min_y + (max_y - min_y), min_x : min_x + (max_x - min_x)]
+                print("these:", max_y, min_y, min_x, max_x)
+                for vertices in boxes[box]:
+                    if vertices[0] >= max_x and vertices[0] > 0 and vertices[0] < 1023:
+                        max_x = vertices[0]
+                    elif vertices[0] < min_x and vertices[0] > 0 and vertices[0] < 1023:
+                        min_x = vertices[0]
+                    if vertices[1] >= max_y and vertices[1] > 0 and vertices[1] < 1023:
+                        max_y = vertices[1]
+                    elif vertices[1] < min_y and vertices[1] > 0 and vertices[0] < 1023:
+                        min_y = vertices[1]
+                print("these:", min_y, min_y + (max_y - min_y), "and", min_x, min_x + (max_x - min_x))
+                im = cv2.imread(my_path , cv2.IMREAD_COLOR)
+                rectangle = im[min_y: min_y + (max_y - min_y), min_x : min_x + (max_x - min_x)]
 
-            cv2.imwrite(name_image + "_" + box + ".jpeg", rectangle)
+                cv2.imwrite(name_image + "_" + box + ".jpeg", rectangle)
+
+
+
+
             #box_poly = shapely.geometry.box(min_x, min_y, max_x, max_y)
                 #polylist.append(box_poly)
         #return polylist
@@ -149,7 +153,7 @@ def intersection_list(polylist: set):
 
 
 
-extract_objects("./dataset/examples/scene0-20_view=0.jpeg")
+extract_objects("./dataset/examples/scene0-20_view=1.jpeg")
 
 
 # in a z-map every pixel in a scene is assigned a 0-255 grayscale value based upon its distance from the camera.
@@ -159,7 +163,7 @@ extract_objects("./dataset/examples/scene0-20_view=0.jpeg")
 # A depth map only contains the distance or Z information for each pixel
 # which in a monochrome (grayscale) 8-bit representation is necessary with values between [0, 255],
 # where 255 represents the closest possible depth value and 0 the most distant possible depth value.
-
+"""
 def point_cloud(image_col, image_depth):
     color_raw = o3d.io.read_image(image_col)
     depth_raw = o3d.io.read_image(image_depth)
@@ -173,3 +177,4 @@ def point_cloud(image_col, image_depth):
     # Flip it, otherwise the pointcloud will be upside down
     pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
     o3d.visualization.draw_geometries([pcd], zoom=0.5)
+"""
