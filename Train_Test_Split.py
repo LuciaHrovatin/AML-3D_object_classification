1f59e6713652d7bbe22054f29ab56fb971315b78
EmmaBenedetti <emma.benedetti@studenti.unitn.it>
2021-08-24 09:47:00 +0200

    added train/test split module

---
+++ Train_Test_Split.py
@@ -0,0 +1,66 @@
+'''
+Split the main dataset final_db.json into a train and a test set, which will
+ be used for analysis and validation of the model
+'''
+
+import json
+import random
+
+"""
+class Split:
+
+    def __init__(self, filename: str):
+        self.filename = filename
+    
+    def split(self):
+        '''
+        splits the original dataset into a train and a test set,
+        given a determined splitting threshold (the variable split_th)
+
+        @param self: the original json database
+        @return train_set, test_set: json files containing training and test dataset
+        '''
+        with open(self.filename, "r") as f:
+            f = json.load(f)
+            n = len(f)
+            
+            random.seed(42)
+            random.shuffle(f)
+
+            split_th = int(0.7*n)
+            train = f[: split_th]
+            test = f[split_th:]
+
+            with open("train_set.json", "w") as train_set:
+                train_set.dump(train)
+            
+            with open("test_set.json", "w") as test_set:
+                test_set.dump(test)
+
+myfile = Split("https://github.com/LuciaHrovatin/AML-3D_object_classification/blob/main/final_db.json")
+myfile.split()
+
+"""
+
+###################################
+###################################
+######### ALTERNATIVELY ###########
+###################################
+###################################
+
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.dataset import Subset
+from sklearn.model_selection import train_test_split
+from data_ingestion import DataIngestion
+
+my_db_class = DataIngestion()
+#dataset = get_dataset("final_db.json")
+dataset = get_dataset(my_db_class.data_storer)
+
+train_index, valid_index = train_test_split(range(len(dataset)), test_size=0.3)
+
+batch_size = 100
+train_dataset = Subset(dataset, train_index)
+train_dataloader = DataLoader(train_dataset, batch_size, shuffle=True)
+valid_dataset   = Subset(dataset, valid_index)
+valid_dataloader = DataLoader(valid_dataset, batch_size, shuffle=False)
