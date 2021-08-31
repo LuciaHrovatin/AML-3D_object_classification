0c5d3d6a122705cd4740ec68e4f96661475fa484
EmmaBenedetti <73296032+EmmaBenedetti@users.noreply.github.com>
2021-08-31 18:26:57 +0200

    Update train_test.py

--- train_test.py
+++ train_test.py
@@ -8,56 +8,85 @@ from data_ingestion import DataIngestion
 
 class Split:
 
-    def __init__(self, data_ingestor: DataIngestion):
+    def __init__(self, data_ingestor: DataIngestion, batch_size: int):
         '''
         Split the main dataset final_db.json into a train and a test set, which will
         be used for analysis and validation of the model
         '''
         self.my_db_class = data_ingestor
+        self.train_loader = None
+        self.test_loader = None
+        self.batch_size = batch_size
 
     def train_test(self):
         dataset = self.get_dataset(self.data_storer)
-        train_index, valid_index = train_test_split(range(len(dataset)), test_size=0.3)
+        train_index, test_index = train_test_split(range(len(dataset)), test_size=0.3)
 
-        batch_size = 100 # perchè settato su 100???
-        train_dataset = Subset(dataset, train_index)
-        train_dataloader = DataLoader(train_dataset, batch_size, shuffle=True)
-        valid_dataset = Subset(dataset, valid_index)
-        valid_dataloader = DataLoader(valid_dataset, batch_size, shuffle=False)
-        return [train_dataloader, valid_dataloader]
+        train_set = Subset(dataset, train_index)
+        self.train_loader = DataLoader(train_dataset, self.batch_size, shuffle=True)
+        test_set = Subset(dataset, test_index)
+        self.test_loader = DataLoader(test_dataset, self.batch_size, shuffle=False)
+        
+    # evaluation
+    def eval_net(self, net, test_loader,epoch_i):
+      net = net.eval()
+
+      acc = 0
+      n_pc = 0
+
+      for batch_it, (x_tr,y_tr) in enumerate(self.test_loader):
+        x_input = torch.FloatTensor(x_tr).cuda()
+        y_input = torch.LongTensor(y_tr).cuda()
+
+        probs = net(x_input)
+
+        acc += (probs.data.cpu().argmax(dim = 1) == y_tr).sum()
+        n_pc += y_tr.shape[0]
+
+        acc = float(acc.data.cpu().numpy()) / n_pc
+
+      print("test accuracy: %.2f" % acc)
 
 
+    #training
+    def train_net(net, n_epochs, train_loader, test_loader):
+      loss_fcn = torch.nn.CrossEntropyLoss()
+      minimizer = torch.optim.Adam(net.parameters(), lr = 0.0001)
 
-"""
-    def split(self):
-        '''
-        splits the original dataset into a train and a test set,
-        given a determined splitting threshold (the variable split_th)
+      losses = []
 
-        @param self: the original json database
-        @return train_set, test_set: json files containing training and test dataset
-        '''
-        with open(self.filename, "r") as f:
-            f = json.load(f)
-            n = len(f)
-            
-            random.seed(42)
-            random.shuffle(f)
+      for epoch_i in range(10):
+        print("EPOCH %s" % epoch_i)
+        with torch.no_grad():
+          eval_net(net, test_loader, epoch_i)
+        net = net.train()
 
-            split_th = int(0.7*n)
-            train = f[: split_th]
-            test = f[split_th:]
+        acc = 0
+        n_pc = 0
 
-            with open("train_set.json", "w") as train_set:
-                train_set.dump(train)
-            
-            with open("test_set.json", "w") as test_set:
-                test_set.dump(test)
+        for batch_it, (x_tr,y_tr) in enumerate[train_loader]:
+          x_input = torch.FloatTensor(x_tr).cuda()
+          y_input = torch.LongTensor(y_tr).cuda()
 
-myfile = Split("https://github.com/LuciaHrovatin/AML-3D_object_classification/blob/main/final_db.json")
-myfile.split()
+          probs = net(x_input)
 
-"""
+          % print(y_input, probs)
+          loss = loss_fcn(probs, y_input).mean()
+
+          minimizer.zero_grad()
+          loss.backward()
+          minimizer.step()
+
+          acc += (probs.data.cpu().argmax(dim = 1) == y_tr).sum()
+          n_pc += y_tr.shape[0]
+
+          losses.append(loss.item())
+
+        ipython_display.display(pl.gcf)
+        ipython_display.clear_output(wait = True)
+        plt.title("Loss")
+        plt.plot(losses)
+        plt.show()
 
 
 
