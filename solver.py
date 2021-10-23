4b951acc52725e8a7fa1c5f1bc285b4bd4bd6918
DESKTOP-B2QPHF7\lucia <luciahrovatin@gmail.com>
2021-10-23 19:10:46 +0200

    tester

--- solver.py
+++ solver.py
@@ -47,9 +47,8 @@ class PointNetClassifier:
             pcd = o3d.geometry.PointCloud()
             pcd.points = o3d.utility.Vector3dVector(train_x[0].T)
             pcd.paint_uniform_color([0, 0, 0])
-            o3d.visualization.draw_geometries([pcd])
+            #o3d.visualization.draw_geometries([pcd])
 
-            # MODIFICARE LA LOSS FUNCTION?
             loss = F.cross_entropy(preds, train_y)
             if trans is not None:
                 loss_reg = feature_transform_regularizer(trans)
@@ -62,15 +61,6 @@ class PointNetClassifier:
             accuracy = torch.sum(correct_classification).float() / train_y.shape[0] * 100
 
             return loss, accuracy
-
-        def test_step(test_x, test_y):
-            test_x = torch.transpose(test_x, 2, 1)
-            with torch.no_grad():
-                preds = model(test_x.float())
-            loss = F.nll_loss(preds, test_y)
-            return loss, preds
-
-        #best_accuracy = 0.0
 
         # for the graph
         losses = []
@@ -109,66 +99,31 @@ class PointNetClassifier:
 
             scheduler.step()
             model.eval()
-            """
-            # It prints a graph at the end of the entire procedure
-            plt.figure(figsize=(10, 5))
-            plt.title("Training Loss")
-            plt.plot(losses, label="train")
-            plt.xlabel("n_epochs")
-            plt.ylabel("Loss")
-            plt.legend()
-            plt.show()
 
-            plt.figure(figsize=(10, 5))
-            plt.title("Accuracy")
-            plt.plot(accuracy_tot, label="train")
-            plt.xlabel("n_epochs")
-            plt.ylabel("Accuracy")
-            plt.legend()
-            plt.show()
-            """
-
-"""
     def test_net(self, test_loader, model):
+        def test_step(test_x, test_y):
+            test_x = torch.transpose(test_x, 2, 1)
+            with torch.no_grad():
+                preds, trans = model(test_x.float())
+            loss = F.cross_entropy(preds, test_y)
+            return loss, preds
+
         for i, batch in enumerate(test_loader):
             data, labels = batch
             batch_loss, preds = test_step(data, labels)
 
-            # for the graph
-            val_losses.append(batch_loss.item())
-
             batch_preds = torch.max(preds, -1).indices
             test_preds.append(batch_preds)
             test_labels.append(labels)
-            total_loss.append(batch_loss)
-
-                # for the graph
-            val.append(statistics.mean(val_losses))
 
             test_preds = torch.cat(test_preds, dim=0).view(-1)
             test_labels = torch.cat(test_labels, dim=0).view(-1)
 
             assert test_preds.shape[0] == test_labels.shape[0]
 
-            loss = sum(total_loss) / len(total_loss)
             correct_classifications = torch.eq(test_labels, test_preds).sum()
             test_accuracy = (correct_classifications / test_labels.shape[0]) * 100
             if test_accuracy > best_accuracy:
                 best_accuracy = test_accuracy
-            print('End of Epoch {0}/{1:03} -> loss: {2:0.05}, test accuracy: {3:0.03} - best accuracy: {4:0.03}'.format(
-                e + 1, self.n_epochs,
-                loss.numpy(),
-                test_accuracy.numpy(),
-                best_accuracy))
 
-            # It prints a graph at the end of the entire procedure
-            plt.figure(figsize=(10, 5))
-            plt.title("Training and Validation Loss")
-            plt.plot(val, label="val")
-            plt.plot(losses, label="train")
-            plt.xlabel("n_epochs")
-            plt.ylabel("Loss")
-            plt.legend()
-            plt.show()
-
-"""
+        print("Final accuracy:{}".format(best_accuracy))
